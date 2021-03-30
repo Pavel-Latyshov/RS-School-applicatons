2fb6ca9dd5f7acb61aff8250e3c0b6e4f015038c
Pavel Latyshov <fussy333@gmail.com>
2021-03-30 23:34:20 +0300

    feat: add finish online block + dark-theme

--- online-zoo/Pages/Main-page/main.js
+++ online-zoo/Pages/Main-page/main.js
@@ -4,6 +4,12 @@ const App = () => {
     let header = document.querySelector('.header-wrapper')
     let toggle = document.querySelector('.view-btn')
     let toggleBall = document.querySelector('.view-ball')
+    let body = document.querySelector('.body-light')
+    let worksWrapper = document.querySelector('.works-wrapper')
+    let worksTitle = document.querySelectorAll('.work-title')
+    let worksText = document.querySelectorAll('.work-text')
+    let worksInfoWrapper = document.querySelector('.works-info-wrapper')
+    let worksAnimal = document.querySelector('.works-animal')
     let toggleFlag = true
 
     toggle.addEventListener('click', () => {
@@ -11,19 +17,37 @@ const App = () => {
             a.forEach(element => {
                 element.classList.add('logo-active')
             });
+            worksTitle.forEach(element => {
+                element.classList.add('light-title')
+            }); 
+            worksText.forEach(element => {
+                element.classList.add('light-text')
+            });
             logo.classList.add('logo-active')
             toggle.classList.add('view-btn-active')
             toggleBall.classList.add('toggle-active')
             header.classList.add('header-wrapper-active')
+            body.classList.add('dark')
+            worksWrapper.classList.add('dark')
+            worksInfoWrapper.classList.add('light-dark-background')
             toggleFlag = false
         } else {
             a.forEach(element => {
                 element.classList.remove('logo-active')
             });
+            worksTitle.forEach(element => {
+                element.classList.remove('light-title')
+            });
+            worksText.forEach(element => {
+                element.classList.remove('light-text')
+            });
             logo.classList.remove('logo-active')
             toggle.classList.remove('view-btn-active')
             toggleBall.classList.remove('toggle-active')
             header.classList.remove('header-wrapper-active')
+            body.classList.remove('dark')
+            worksWrapper.classList.remove('dark')
+            worksInfoWrapper.classList.remove('light-dark-background')
             toggleFlag = true
         }
     })

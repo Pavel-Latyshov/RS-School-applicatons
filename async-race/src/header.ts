4903040017d43f5112b6c916e2d20cd17c5a3ebe
Pavel-Latyshov <fussy333@gmail.com>
2021-06-09 15:35:48 +0300

    feat: add header buttons

---
+++ async-race/src/header.ts
@@ -0,0 +1,11 @@
+const Header = () => {
+    const body: HTMLElement = document.querySelector('body')
+//     body.innerHTML = `<div class="header_wrapper">
+//     <div class="header_container">
+//         <div class="garage_btn__wrapper"></div>
+//         <div class="winner_btn__wrapper"></div>
+//     </div>
+// </div>`
+}
+
+export default Header
--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,2 +1,4 @@
+import Header from './header';
 import './style/index.css';
 
+Header()
